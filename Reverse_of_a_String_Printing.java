554219f62d3abacc1604bd5bd0843663d9a1ac25
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2026-05-14 18:37:44 +0530

    Add files via upload

---
+++ Reverse_of_a_String_Printing.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s =sc.nextLine();
+        for(int i=s.length()-1;i>=0;i--){
+            char c = s.charAt(i);
+            System.out.print(c);
+        }
+	}    
+}
