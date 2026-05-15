5ccdf8c4ee2a2ea04d7e9b5d59bdc29daaaf9491
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2026-05-15 08:57:37 +0530

    Update Factorial_num.java

--- Numeric_based/Factorial_num.java
+++ Numeric_based/Factorial_num.java
@@ -1,23 +1,22 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int f=1;
-		for(int i=1;i<=num;i++){
-           f*=i;
-		}
-		if(num==0|num==1){
-		    System.out.println(f);
-		}
-	    else{
-	        System.out.println(f);
-	    }
-	        
+
+class Codechef {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int num = sc.nextInt();
+            
+            // Single condition: Only calculate if number is 0 or positive
+            if (num >= 0) {
+                long f = 1;
+                for (int i = 1; i <= num; i++) {
+                    f *= i;
+                }
+                System.out.println(f);
+            } else {
+                System.out.println("Invalid Input"); 
+            }
+        }
     }
 }
